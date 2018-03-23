d3526fb9b896f386a9a2bf969ff0256629b8e4de
Unknown <88376827@qq.com>
2018-03-23 16:10:50 +0800

    changes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 now=`date --date='0 days ago' "+%Y%m%d%H%M%S"`
 
 echo "=====Step1 jmeter"
-cmd2="/jmeter/apache-jmeter-3.3/bin/jmeter -n -t ${ENV_JMX_FILE_NAME} -l ${ENV_JMX_FILE_NAME}_${now}.jtl -e -o ${ENV_JMX_FILE_NAME}_${now} -R${ENV_SLAVES}"
+cmd2="/jmeter/apache-jmeter-3.3/bin/jmeter -n -t ${ENV_JMX_FILE_NAME}.jmx -l ${ENV_JMX_FILE_NAME}_${now}.jtl -e -o ${ENV_JMX_FILE_NAME}_${now} -R${ENV_SLAVES}"
 eval ${cmd2}
 
 echo "=====Step2 scp"
@@ -19,10 +19,10 @@ eval ${cmd4}
 
 #scp report
 treport="/home/core/nginx/html/jmetertest/report"
-cmd5="ssh -i -o \"StrictHostKeyChecking no\" hostkey core@${ENV_PROXY_IP} \"[ -d ${treport}/ ] && echo ok || mkdir -p ${treport}/\""
+cmd5="ssh -i hostkey -o \"StrictHostKeyChecking no\" hostkey core@${ENV_PROXY_IP} \"[ -d ${treport}/ ] && echo ok || mkdir -p ${treport}/\""
 eval ${cmd5}
 
-cmd7="scp -i -o \"StrictHostKeyChecking no\" hostkey -r ${ENV_JMX_FILE_NAME}_${now} core@${ENV_PROXY_IP}:${treport}/"
+cmd7="scp -i hostkey -o \"StrictHostKeyChecking no\" hostkey -r ${ENV_JMX_FILE_NAME}_${now} core@${ENV_PROXY_IP}:${treport}/"
 eval ${cmd6}
 
 echo "=====End JMeter Test ${ENV_JMX_FILE_NAME} on $now"
